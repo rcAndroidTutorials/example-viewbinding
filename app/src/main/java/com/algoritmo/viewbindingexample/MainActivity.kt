60db9ce4e0a7a8e5f3e1b9e1465275bce3320e3e
Ricardo Champa <rchampa.san@gmail.com>
2020-05-16 12:11:50 +0200

    first commit

---
+++ app/src/main/java/com/algoritmo/viewbindingexample/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.algoritmo.viewbindingexample
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.algoritmo.viewbindingexample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btLogin.setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
+    }
+}
