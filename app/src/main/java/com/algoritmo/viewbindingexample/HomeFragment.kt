6451e38b03ddd9f17ea1dc614e98f2b42356dcb0
Ricardo Champa <rchampa.san@gmail.com>
2020-05-19 00:02:52 +0200

    Nuevo video

--- app/src/main/java/com/algoritmo/viewbindingexample/HomeFragment.kt
+++ app/src/main/java/com/algoritmo/viewbindingexample/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.algoritmo.viewbindingexample.databinding.FragmentHomeBinding
 
@@ -16,9 +17,16 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        binding.tvTitle.text = "Hello View Binding from fragment"
+        binding.tvTitle.text = "Hola Algoritmo Final!!"
+        binding.btDespedida.setOnClickListener {
+            Toast.makeText(
+                requireContext(),
+                "Gracias por ver el video",
+                Toast.LENGTH_SHORT).show()
+        }
         return binding.root
     }
+
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
--- app/src/main/java/com/algoritmo/viewbindingexample/MainActivity.kt
+++ app/src/main/java/com/algoritmo/viewbindingexample/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.algoritmo.viewbindingexample
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.algoritmo.viewbindingexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,8 +13,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.btRegister.setOnClickListener {
+            Toast.makeText(this, "Registro aún no disponible", Toast.LENGTH_SHORT).show()
+        }
         binding.btLogin.setOnClickListener {
             startActivity(Intent(this, HomeActivity::class.java))
         }
+
+//        setContentView(R.layout.activity_main)
+//        val loginButton = findViewById<Button>(R.id.btLogin)
+//        loginButton.setOnClickListener {
+//            startActivity(Intent(this, HomeActivity::class.java))
+//        }
     }
 }
